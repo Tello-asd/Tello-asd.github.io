e2e1e981f2c87584b4f0ccf9e4784d06d93e7c90
AlexLTM <alopeztello@yahoo.es>
2020-06-11 12:15:43 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,37 +1,36 @@
-const cacheName = 'pwa-conf-v1';
-const staticAssets = ['./', '/index.html', 'js/app.js', 'css/styles.css'];
-self.addEventListener('install', async event => {
-  const cache = await caches.open(cacheName);
-  await cache.addAll(staticAssets);
+const version = '0.6.4';
+
+self.addEventListener('install', e => {
+    console.log('install');
+    const timeStamp = Date.now();
+    e.waitUntil(
+        caches
+            .open('todoList')
+            .then(cache => {
+                return cache.addAll([
+                    `/`,
+                    `index.html?timestamp=${timeStamp}`,
+                    `/js/bundle.js?timestamp=${timeStamp}`,
+                ])
+                .then(() => self.skipWaiting());
+            })
+    )
 });
-// Optional: clents.claim() makes the service worker take over the current page
-// instead of waiting until next load. Useful if you have used SW to prefetch content
-// that's needed on other routes. But potentially dangerous as you are still running the
-// previous version of the app, but with new resources.
-self.addEventListener('activate', event => {
-  event.waitUntil(self.clients.claim());
+
+self.addEventListener('activate',  event => {
+    console.log('activate');
+    event
+        .waitUntil(self.clients
+        .claim());
 });
+
 self.addEventListener('fetch', event => {
-  const req = event.request;
-if (/.*(json)$/.test(req.url)) {
-    event.respondWith(networkFirst(req));
-  } else {
-    event.respondWith(cacheFirst(req));
-  }
+    console.log('fetch');
+    event
+        .respondWith(
+        caches.match(event.request, {ignoreSearch:true})
+            .then(response => {
+                return response || fetch(event.request);
+            })
+    );
 });
-async function cacheFirst(req) {
-  const cache = await caches.open(cacheName);
-  const cachedResponse = await cache.match(req);
-  return cachedResponse || networkFirst(req);
-}
-async function networkFirst(req) {
-  const cache = await caches.open(cacheName);
-  try {
-    const fresh = await fetch(req);
-    cache.put(req, fresh.clone());
-    return fresh;
-  } catch (e) {
-    const cachedResponse = await cache.match(req);
-    return cachedResponse;
-  }
-}
