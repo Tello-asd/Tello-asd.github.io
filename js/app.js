689ddbf1a14b5b75fd06d68d09d1f3f28449619f
AlexLTM <alopeztello@yahoo.es>
2020-06-11 14:18:56 +0200

    index changes

--- js/app.js
+++ js/app.js
@@ -127,18 +127,6 @@ list.addEventListener("click", function(event){
 });
 
 /******************CONNECT TO serviceWorker************************/
-window.addEventListener('load', e => {
-  registerSW();
-});
 
-async function registerSW() {
-  if ('serviceWorker' in navigator) {
-    try {
-      await navigator.serviceWorker.register('sw.js');
-    } catch (e) {
-      alert('ServiceWorker registration failed. Sorry about that.');
-    }
-  } else {
-    document.querySelector('.alert').removeAttribute('hidden');
-  }
-}
+
+ 
